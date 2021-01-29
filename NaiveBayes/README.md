759d20708ea5959a69ada3ce64138320c3d3a47c
Giotto Frean <giotto.frean@gmail.com>
2021-01-29 23:02:08 +1300

    initial code for NB, generates data

--- NaiveBayes/README.md
+++ NaiveBayes/README.md
@@ -16,3 +16,16 @@ This is represented by the diagram below, which has the "true" probabilities: <b
 
 <br>
 Using the above model data is generated, and a Naive Bayes classifier trained with it.<br>
+
+Expected output: <br>
+Generating Data (up to 10 rows): <br>
+weather: clear      temp: 16.481     raincoats: few <br>
+weather: sun        temp: 15.834     raincoats: none <br>
+weather: clear      temp: 22.002     raincoats: none <br>
+weather: rain       temp: 14.784     raincoats: lots <br>
+weather: sun        temp: 17.916     raincoats: none <br>
+weather: sun        temp: 20.456     raincoats: few <br>
+weather: sun        temp: 16.528     raincoats: none <br>
+weather: sun        temp: 17.38      raincoats: none <br>
+weather: clear      temp: 16.731     raincoats: none <br>
+weather: rain       temp: 18.822     raincoats: lots <br>
--- NaiveBayes/main.py
+++ NaiveBayes/main.py
@@ -1,45 +1,28 @@
 import numpy as np
 
-number_of_elements = 10
-array = np.random.randint(0,10,number_of_elements) # make a random array
-sorted_array_copy_for_validation = np.sort(array.copy()) # store a copy, to check the results.
+# Toy problem parameters:
+true_W_probs = np.array([0.2,0.3,0.5])
+true_T_means_given_W = np.array([22,15,17])
+true_T_stds_given_W = np.array([4.2,3.4,3.9])
+true_R_probs_given_W = np.array([[0.9,0,0.1],[0.1,0.1,0.7],[0,0.8,0.2],[0,0.1,0]])
 
-print(array, "  <- original array")
-print("")
+# Settings for generating data:
+number_of_data_points = 15
 
-# This is a special method to print the array in sections, to show which bits are being operated on (before and after). 
-# Good for debugging, and checking process.
-def print_array_sections(array, number_of_sections, gap_size):
-	for section in range(number_of_sections):
-		print(array[section * gap_size:(section+1) * gap_size],end="")
-	print("")
+data = np.zeros((number_of_data_points,3))
+print("Generating Data (up to 10 rows):")
+for i in range(number_of_data_points):
+	# Generate Data
+	w_i = np.random.choice(np.arange(3),p=true_W_probs)
+	t_i = np.random.normal(true_T_means_given_W[w_i],true_T_stds_given_W[w_i])
+	r_i = np.random.choice(np.arange(4),p=true_R_probs_given_W[:,w_i])
+	data[i,0]=w_i
+	data[i,1]=t_i
+	data[i,2]=r_i
 
-# Main loop: 
-for depth in range(1,int(np.ceil(np.log2(number_of_elements)))+1): # At most log_2(number_of_elements)+1 depth needed. 
-	gap_size = 2**depth # show many items are being merged/sorted at one time. 
-	number_of_sections = int(np.ceil(number_of_elements/gap_size))
-	print_array_sections(array, number_of_sections, gap_size) # show array in sections, before processing the depth. 
-
-	for section in range(number_of_sections):
-		lower = section * gap_size
-		upper = section * gap_size + int(gap_size/2)
-		for i in range(min(gap_size,number_of_elements-lower)-1):
-			if(upper>=number_of_elements or upper==(section+1)*gap_size or lower==upper): # special case ending conditions.
-				break
-			else:
-				if array[lower]<=array[upper]:
-					lower += 1
-				else: 
-					temp = array[upper]
-					for j in range(upper,lower,-1): # push all elements forward 1 space, to make room for insertion.  
-						array[j]=array[j-1]
-					array[lower] = temp
-					lower += 1
-					upper += 1
-	
-	print_array_sections(array, number_of_sections, gap_size) # show array after this step. 
-
-print(array, "  <- final sorted array")
-assert((array==sorted_array_copy_for_validation).all()) # Check the solution was correct. 
-
-
+	# Printout
+	if(i<10):
+		w_i_name = ["sun","rain","clear"][w_i]
+		t_i_rounded = np.round(t_i,3)
+		r_i_name = ["none", "few","lots","all"][r_i]
+		print(str.format("weather: {:<10} temp: {:<10} raincoats: {:<10}", w_i_name,t_i_rounded,r_i_name))
